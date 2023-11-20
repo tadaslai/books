9e90fd1ab4d84b0fb1dbd1672cf05b251a76bb91
Tadas <tadaslaiskonis@gmail.com>
2023-11-20 11:41:47 +0200

    Requirements commit

--- Backend/settings.py
+++ Backend/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -22,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b*zg6-elk#yw8b@&983z-#e)8avkrlvzhsjz2z1qq-n+)z5wiy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['http://localhost:3000', '127.0.0.1', 'localhost']
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_METHODS = ['POST', "PUT", "DELETE"]
 CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
@@ -131,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
